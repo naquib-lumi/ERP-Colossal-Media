223637e4f3bdb1dee11d0d0bf220c3a1bb4296d7
root <root@colossal360.com.my>
2025-08-04 07:14:44 +0000

    fix

--- resources/views/sales/dashboard.blade.php
+++ resources/views/sales/dashboard.blade.php
@@ -3,9 +3,11 @@
 @section('title', 'Dashboard Overview')
 @section('content')
 <h1>Dashboard Overview</h1>
-    <p>Welcome, {{ auth()->user()->name }}! This is your salesperson dashboard.</p>
+    <p>Welcome, {{ auth()->user()->name }}! Here's your sales activity summary.</p>
 <div class="container-xxl flex-grow-1 container-p-y">
-    <h4 class="fw-bold py-3 mb-4">Dashboard Overview</h4>
+    
+    
+<h4 class="fw-bold py-3 mb-4">Dashboard Overview</h4>
     <div class="row">
         <div class="col-12">
             <div class="card">
