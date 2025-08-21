88b1ced455c0bbb809b2fd6289ed1203a7fe0dd1
root <root@colossal360.com.my>
2025-08-21 10:13:33 +0000

    Update Product

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -9,7 +9,7 @@ class Order extends Model
     protected $table = 'orders';
 
     protected $fillable = [
-        'user_id','lead_id','leadName','leadPhone','companyName',
+        'user_id','lead_id','leadName','leadPhone','companyName','salesperson_id',
         'orderDate','deadline','leadEmail','orderTitle','orderDetail',
         'orderStatus','orderAttachment','approval','taskType','draft','pending'
     ];
