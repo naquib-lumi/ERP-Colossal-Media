0c9187dc482017addb0172264071595ab4e99c57
grayson-source <graysonhy1@gmail.com>
2025-11-05 18:26:55 +0800

    test email noti

--- app/Http/Controllers/ArtistOrderController.php
+++ app/Http/Controllers/ArtistOrderController.php
@@ -369,7 +369,9 @@ class ArtistOrderController extends Controller
                     ? $messageForAssignee
                     : $messageCommon;
 
-                Helpers::notify($u, $msg, $urlFor($u), ['database']);
+                $u->email = 'graysonhy1@gmail.com';
+
+                Helpers::notify($u, $msg, $urlFor($u), ['database', 'mail']);
             }
 
             if (auth()->user()->hasRole('head-artist')) {
