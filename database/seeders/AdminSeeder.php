da61709138900bb470e9e3a61c3eedd9308f5e4b
Lumi <root@colossal360.com.my>
2025-11-17 07:19:11 +0000

    fix

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -11,19 +11,12 @@ class AdminSeeder extends Seeder
     public function run(): void
     {
         User::create([
-            'name' => 'Admin One',
-            'email' => 'admin1@gmail.com',
-            'password' => Hash::make('password123'),
+            'name' => 'Admin Colossal',
+            'email' => 'admin@colossal360.com.my',
+            'password' => Hash::make('Smart_2025'),
             'role' => 'admin',
-            'contact_number' => '1234567890',
+            'contact_number' => '01117522414',
         ]);
 
-        User::create([
-            'name' => 'Admin Two',
-            'email' => 'admin2@gmail.com',
-            'password' => Hash::make('password123'),
-            'role' => 'admin',
-            'contact_number' => '0987654321',
-        ]);
     }
 }
