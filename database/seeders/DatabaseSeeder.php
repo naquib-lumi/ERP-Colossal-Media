8c0e3171edb2c81972411c51bc1e9287eaeb47ed
Lumi <root@colossal360.com.my>
2025-11-17 07:13:11 +0000

    fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,19 +8,19 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        $this->call(SalesPersonSeeder::class);
-        $this->call(LeadSeeder::class);
-        $this->call(MeetingSeeder::class);
-        $this->call(ArtistSeeder::class);
+        //$this->call(SalesPersonSeeder::class);
+        //$this->call(LeadSeeder::class);
+        //$this->call(MeetingSeeder::class);
+        //$this->call(ArtistSeeder::class);
         //$this->call(OrdersTableSeeder::class);
        // $this->call(ProductsTableSeeder::class);
         $this->call(ProductTaskTypeSeeder::class);
-        $this->call(OperationSeeder::class);
-        $this->call(DataEntrySeeder::class);
+        //$this->call(OperationSeeder::class);
+        //$this->call(DataEntrySeeder::class);
         //$this->call(FulfillmentProgressSeeder::class);
         $this->call(BossSeeder::class);
         $this->call(MaterialSeederLatest::class);
         $this->call(MachineSeeder::class);
-       // $this->call(AdminSeeder::class);
+        $this->call(AdminSeeder::class);
     }
 }
