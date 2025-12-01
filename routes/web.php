3e4592bd3b60cf05ccd107eb4237c0d0bcfc1519
grayson-source <graysonhy1@gmail.com>
2025-12-01 14:07:06 +0800

    update route crach'

--- routes/web.php
+++ routes/web.php
@@ -557,7 +557,7 @@ Route::delete('/admin/installation/permit/{permit}', [AdminController::class,'in
         Route::get('/boss/orders/assignees/search', [BossOrderController::class, 'searchOrderArtists'])->name('boss.orders.assignees.search');
         Route::post('/boss/orders/{order}/edit', [BossOrderController::class, 'assign'])->name('boss.orders.assigns');
 
-        Route::get('/data-entry/users', [ArtistController::class, 'dataEntryUsers'])->name('dataEntry.users');
+        // Route::get('/data-entry/users', [ArtistController::class, 'dataEntryUsers'])->name('boss.dataEntry.users');
     });
  
 });
