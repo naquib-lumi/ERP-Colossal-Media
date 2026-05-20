d068cba9b2dcdc5d83782131ded098fef8840d09
grayson-source <graysonhy1@gmail.com>
2026-05-20 11:48:45 +0800

    test email noti in temp email

--- app/Models/User.php
+++ app/Models/User.php
@@ -64,7 +64,7 @@ class User extends Authenticatable
     public function routeNotificationForMail($notification = null)
 {
     if (app()->environment(['local', 'staging'])) {
-        return 'yumikohuii8399@gmail.com';
+        return 'graysonhy1@gmail.com';
     }
     return $this->email;
 }
